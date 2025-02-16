178a3e755e72e75bded32992ca829479f29ca0ab
Noah Bright <noah.bright.1@gmail.com>
2025-02-16 11:21:11 -0500

    gridlines

--- game_of_life.js
+++ game_of_life.js
@@ -45,7 +45,99 @@
         return program;
     }
     
-    const gl = get_webgl_context("#gameOfLifeCanvas", "webgl");
+    const gl = get_webgl_context("#gameOfLifeCanvas", "webgl2");
     const canvas = document.querySelector("#gameOfLifeCanvas");
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 0.0, 0.0, 1.0);
+    console.log(`Max texture size ${gl.getParameter(gl.MAX_TEXTURE_SIZE)}`);
+    const bytes_per_float = Float32Array.BYTES_PER_ELEMENT;
 
+    /////////////////////////////////
+    // Game of Life game state
+    /////////////////////////////////
+    const pixels_per_square = 16;
+    const gol_width = Math.floor(canvas.width / pixels_per_square);
+    const gol_height = Math.floor(canvas.height / pixels_per_square);
+    const num_cells = gol_width * gol_height;
+    const dx = 2.0 / gol_width; // gl screen space
+    const dy = 2.0 / gol_height;
+
+    /////////////////////////////////
+    // Draw gridlines
+    /////////////////////////////////
+
+    // to draw a straight line, we need a start and an endpoint
+    // in 2D, each point needs an x and y
+    // for a straight line of constant y spanning the whole
+    // screen, the coords are
+    // (x_min, y) and (x_max, y)
+    // in opengl screenspace, x_max/x_min are +/-1.0
+    let gridline_vertices = [];
+
+    // lines of constant x with y = +/- 1.0
+    for(let x = -1.0; x < 1.0; x += dx){
+        gridline_vertices.push(x);
+        gridline_vertices.push(-1.0);
+        gridline_vertices.push(x);
+        gridline_vertices.push(1.0);
+    }
+
+    for(let y = -1.0; y < 1.0; y += dy){
+        gridline_vertices.push(-1.0);
+        gridline_vertices.push(y);
+        gridline_vertices.push(1.0);
+        gridline_vertices.push(y);
+    }
+
+    const lines_vertex_source = `#version 300 es
+        in vec2 a_pos;
+
+        void main(){
+            gl_Position = vec4(a_pos, 0.0, 1.0);
+        }
+    `
+
+    const lines_fragment_source = `#version 300 es
+        precision mediump float;
+        out vec4 fragColor;
+
+        void main(){
+            fragColor = vec4(0.3, 0.4, 0.0, 1.0);
+        }
+    `
+
+    const lines_program = create_and_link_shaders(gl, lines_vertex_source, lines_fragment_source);
+    gl.useProgram(lines_program);
+    const lines_position_attribute_location = gl.getAttribLocation(lines_program, "a_pos");
+
+    const lines_vao = gl.createVertexArray();
+    gl.bindVertexArray(lines_vao);
+
+    const lines_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, lines_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(gridline_vertices), gl.STATIC_DRAW);
+
+    gl.enableVertexAttribArray(lines_position_attribute_location);
+    gl.vertexAttribPointer(lines_position_attribute_location,
+        2, gl.FLOAT, false, 2*bytes_per_float, 0
+    );
+
+    function draw_lines(){
+        gl.useProgram(lines_program);
+        gl.bindVertexArray(lines_vao);
+        gl.drawArrays(gl.LINES, 0, 2 * (gol_width + gol_height));
+    }
+
+    /////////////////////////////////
+    // Main render loop
+    /////////////////////////////////
+
+    function render(){
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        draw_lines();
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
 })();
